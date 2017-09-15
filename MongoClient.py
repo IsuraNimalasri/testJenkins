0be53f7c8f43bae99514a3687113ec6c4431825f
Isura Nimalasiri <isuranimalasiri>
2017-09-15 16:12:01 +0530

    vcs.xml update

---
+++ MongoClient.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+import CutReportMaker as crm
+
+
+#  create mongo client .
+
+client = MongoClient('localhost', 27017)
+db = client['blzdb']
+
+# Make CutReport
+
+scd_dates = ["2017-09-15"]
+crm.MakeCutReport(db,scd_dates,"isura")
+
+
+
+
+
+
+
